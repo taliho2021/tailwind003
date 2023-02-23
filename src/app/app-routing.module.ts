ace8bc7c7384cfa054af5dab8629654b875daa3d
Tal Yo <tal@analink.com>
2023-02-23 08:17:50 -0500

    Deleted address and dash components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { RouterModule, Routes } from '@angular/router';
 
-import { AddressComponent } from './address/address.component';
-import { DashComponent } from './dash/dash.component';
+import { AiGridComponent } from './ai-grid/ai-grid.component';
 import { DragDropComponent } from './drag-drop/drag-drop.component';
 import { FlexFormComponent } from './flex-form/flex-form.component';
+import { FlexGridComponent } from './flex-grid/flex-grid.component';
 import { GridFormComponent } from './grid-form/grid-form.component';
 import { HomeComponent } from './home/home.component';
 import { NavComponent } from './nav/nav.component';
@@ -11,24 +11,12 @@ import { NgModule } from '@angular/core';
 import { PicturesComponent } from './pictures/pictures.component';
 import { Tailwind01Component } from './tailwind01/tailwind01.component';
 import { Tailwind02Component } from './tailwind02/tailwind02.component';
-import { Child01Component } from './child01/child01.component';
-import { AiGridComponent } from './ai-grid/ai-grid.component';
-import { FlexGridComponent } from './flex-grid/flex-grid.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
-  {path:'address', component: AddressComponent,
-    children: [{
-    path: 'child', component:Child01Component
-  }]},
   {path:'nav', component: NavComponent},
   {path: 'one', component: Tailwind01Component},
   {path: 'two', component: Tailwind02Component},
-  {path: 'dash', component: DashComponent,
-    children: [{
-      path: 'child', component:Child01Component
-  }]
-  },
   {path: 'drag', component: DragDropComponent},
   {path: 'pictures', component: PicturesComponent},
   {path: 'flex', component: FlexFormComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { AddressComponent } from './address/address.component';
 import { AiGridComponent } from './ai-grid/ai-grid.component';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { Child01Component } from './child01/child01.component';
-import { DashComponent } from './dash/dash.component';
 import { DragDropComponent } from './drag-drop/drag-drop.component';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { FlexFormComponent } from './flex-form/flex-form.component';
+import { FlexGridComponent } from './flex-grid/flex-grid.component';
 import { GridFormComponent } from './grid-form/grid-form.component';
 import { HomeComponent } from './home/home.component';
 import { LayoutModule } from '@angular/cdk/layout';
@@ -29,19 +28,16 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { NavComponent } from './nav/nav.component';
 import { NgModule } from '@angular/core';
 import { PicturesComponent } from './pictures/pictures.component';
+import { SubGridComponent } from './sub-grid/sub-grid.component';
 import { Tailwind01Component } from './tailwind01/tailwind01.component';
 import { Tailwind02Component } from './tailwind02/tailwind02.component';
-import { FlexGridComponent } from './flex-grid/flex-grid.component';
-import { SubGridComponent } from './sub-grid/sub-grid.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddressComponent,
     NavComponent,
     Tailwind01Component,
     Tailwind02Component,
-    DashComponent,
     HomeComponent,
     DragDropComponent,
     PicturesComponent,
